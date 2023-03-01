63fae5f1f689f7476646bd39cdcc88a24078bde4
Sam Verschueren <sam.verschueren@gmail.com>
2023-03-01 17:07:22 +0100

    chore: small tweaks

--- setup-files/patch/suppress-maintenance-mode.mjs
+++ setup-files/patch/suppress-maintenance-mode.mjs
@@ -2,12 +2,12 @@
  * Patches an issue with an invalid `main` field in the `cloudform` package. This was fixed upstream,
  * but is hard to get it updated in the Amplify CLI itself because it depends on it in various different
  * places.
- * 
+ *
  * @see https://github.com/aws-amplify/amplify-cli/issues/9939
  * @see https://github.com/bright/cloudform/issues/70
  */
-import * as fs from 'node:fs'
-import * as path from 'node:path'
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
 const suppressWarning = `require('aws-sdk/lib/maintenance_mode_message').suppress = true;`;
 
