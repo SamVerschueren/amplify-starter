c122f8cd5c62683b03e227c5c129be66b842a4c3
Sam Verschueren <sam.verschueren@gmail.com>
2023-03-01 11:26:04 +0100

    chore: fix typo

--- setup-files/patch/supress-maintenance-mode.mjs
+++ setup-files/patch/supress-maintenance-mode.mjs
@@ -9,13 +9,13 @@
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 
-const supressWarning = `require('aws-sdk/lib/maintenance_mode_message').suppress = true;`;
+const suppressWarning = `require('aws-sdk/lib/maintenance_mode_message').suppress = true;`;
 
 const filePath = path.resolve('node_modules/.bin/amplify');
 
 let contents = fs.readFileSync(filePath, 'utf8');
 
 // supress the warning by appending the rule right after the `eslint-disable` line
-contents = contents.replace(/(\/\* eslint-disable .*? \*\/)/, `$1\n${supressWarning}`);
+contents = contents.replace(/(\/\* eslint-disable .*? \*\/)/, `$1\n${suppressWarning}`);
 
 fs.writeFileSync(filePath, contents);
