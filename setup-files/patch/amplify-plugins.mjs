77c646fd5508c17613d77a2bede18106a7cfdbae
Sam Verschueren <sam.verschueren@gmail.com>
2023-03-06 08:42:49 +0100

    chore: fix loading local plugins

--- setup-files/patch/amplify-plugins.mjs
+++ setup-files/patch/amplify-plugins.mjs
@@ -1,5 +1,12 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
+import * as childProcess from 'node:child_process';
+import * as url from 'node:url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+// initialize the `~/.amplify` folder with it's plugins
+initialize();
 
 const filepath = path.resolve(process.env.HOME, '.amplify/plugins.json');
 const read = fs.readFileSync(filepath, 'utf-8');
@@ -11,6 +18,27 @@ if (parsed.pluginDirectories.includes(key)) {
   process.exit(0);
 }
 
+/**
+ * Add `packaged-node-modules` to the plugin directories, this will enforce Amplify to also
+ * read in the local installed `node_modules`.
+ * 
+ * By setting the `lastScanTime` to something in the past, we can retrigger initialization in
+ * order for Amplify to now also pick up the local node module plugins.
+ */
 parsed.pluginDirectories = [...parsed.pluginDirectories, key];
+parsed.lastScanTime = "2000-01-01T00:00:00.000Z";
 
 fs.writeFileSync(filepath, JSON.stringify(parsed, null, 2), 'utf-8');
+
+// initialize again with the updated `plugins.json`
+initialize();
+
+function initialize() {
+  childProcess.spawnSync('amplify', ['version'], {
+    stdio: 'ignore',
+    env: {
+      ...process.env,
+      PATH: `${process.env.PATH}:${path.resolve(__dirname, '../../node_modules/.bin')}`
+    }
+  });
+}
