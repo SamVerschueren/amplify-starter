a349dd7ca72ee3d30fe28d83ac303ff0552115eb
Sam Verschueren <sam.verschueren@gmail.com>
2023-03-07 17:31:25 +0100

    chore: patch amplify plugins more easily

--- setup-files/patch/amplify-plugins.mjs
+++ setup-files/patch/amplify-plugins.mjs
@@ -1,44 +1,33 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
-import * as childProcess from 'node:child_process';
-import * as url from 'node:url';
-
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-
-// initialize the `~/.amplify` folder with it's plugins
-initialize();
 
 const filepath = path.resolve(process.env.HOME, '.amplify/plugins.json');
-const read = fs.readFileSync(filepath, 'utf-8');
-const parsed = JSON.parse(read);
 
-const key = 'packaged-node-modules';
-
-if (parsed.pluginDirectories.includes(key)) {
-  process.exit(0);
-}
+fs.mkdirSync(path.dirname(filepath), { recursive: true });
 
 /**
- * Add `packaged-node-modules` to the plugin directories, this will enforce Amplify to also
- * read in the local installed `node_modules`.
- * 
- * By setting the `lastScanTime` to something in the past, we can retrigger initialization in
- * order for Amplify to now also pick up the local node module plugins.
+ * Write a `plugins.json` file with the default settings. The only thing we add is
+ * `packaged-node-modules` which will force Amplify to also look into the local
+ * `node_modules` directory. We also set the `lastScanTime` to something in the past
+ * to force a new scan.
  */
-parsed.pluginDirectories = [...parsed.pluginDirectories, key];
-parsed.lastScanTime = '2000-01-01T00:00:00.000Z';
-
-fs.writeFileSync(filepath, JSON.stringify(parsed, null, 2), 'utf-8');
-
-// initialize again with the updated `plugins.json`
-initialize();
-
-function initialize() {
-  childProcess.spawnSync('amplify', ['version'], {
-    stdio: 'ignore',
-    env: {
-      ...process.env,
-      PATH: `${process.env.PATH}:${path.resolve(__dirname, '../../node_modules/.bin')}`
-    }
-  });
-}
+fs.writeFileSync(filepath, JSON.stringify(
+  {
+    pluginDirectories: [
+      'cli-local-node-modules',
+      'cli-parent-directory',
+      'global-node-modules',
+      'packaged-node-modules'
+    ],
+    pluginPrefixes: [
+      'amplify-'
+    ],
+    userAddedLocations: [],
+    lastScanTime: '2000-01-01T00:00:00.000Z',
+    maxScanIntervalInSeconds: 86400,
+    plugins: {},
+    excluded: {}
+  },
+  undefined,
+  '  '
+));
