37f6fc7b09c377c72ab38ac2469f935e78f78c44
Sam Verschueren <sam.verschueren@gmail.com>
2023-03-08 11:33:09 +0100

    chore: modify plugin-platform instead of writing plugins.json

--- setup-files/patch/amplify-plugins.mjs
+++ setup-files/patch/amplify-plugins.mjs
@@ -1,33 +1,19 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 
-const filepath = path.resolve(process.env.HOME, '.amplify/plugins.json');
+const filePath = path.resolve('node_modules/amplify-cli-core/lib/context/plugin-platform.js');
 
-fs.mkdirSync(path.dirname(filepath), { recursive: true });
+let contents = fs.readFileSync(filePath, 'utf8');
 
 /**
- * Write a `plugins.json` file with the default settings. The only thing we add is
- * `packaged-node-modules` which will force Amplify to also look into the local
- * `node_modules` directory. We also set the `lastScanTime` to something in the past
- * to force a new scan.
+ * Update the list of `pluginDirectories` to also include `packaged-node-modules`. An alternative
+ * approach is by manually writing a `~/.amplify/plugins.json` file.
+ * 
+ * @see https://github.com/SamVerschueren/amplify-starter/blob/a349dd7ca72ee3d30fe28d83ac303ff0552115eb/setup-files/patch/amplify-plugins.mjs
  */
-fs.writeFileSync(filepath, JSON.stringify(
-  {
-    pluginDirectories: [
-      'cli-local-node-modules',
-      'cli-parent-directory',
-      'global-node-modules',
-      'packaged-node-modules'
-    ],
-    pluginPrefixes: [
-      'amplify-'
-    ],
-    userAddedLocations: [],
-    lastScanTime: '2000-01-01T00:00:00.000Z',
-    maxScanIntervalInSeconds: 86400,
-    plugins: {},
-    excluded: {}
-  },
-  undefined,
-  '  '
-));
+contents = contents.replace(
+  /this\.pluginDirectories = \[(.*?)\];/, 
+  `this.pluginDirectories = [$1, constants_1.constants.PACKAGED_NODE_MODULES];`
+);
+
+fs.writeFileSync(filePath, contents);
